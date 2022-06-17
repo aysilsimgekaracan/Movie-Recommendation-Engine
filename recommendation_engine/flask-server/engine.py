affdbfd266b3792a8cca2d0f1a59277397c76cdd
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2022-06-17 11:27:46 +0300

    engine.py

---
+++ recommendation_engine/flask-server/engine.py
@@ -0,0 +1,67 @@
+
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+
+import warnings; warnings.simplefilter('ignore')
+
+md = pd. read_csv('../the-movies-dataset/data_elastic.csv')
+
+vote_counts = md[md['vote_count'].notnull()]['vote_count'].astype('int')
+vote_averages = md[md['vote_average'].notnull()]['vote_average'].astype('int')
+C = vote_averages.mean()
+
+m = vote_counts.quantile(0.95)
+
+md['year'] = pd.to_datetime(md['release_date'], errors='coerce').apply(lambda x: str(x).split('-')[0] if x != np.nan else np.nan)
+
+qualified = md[(md['vote_count'] >= m) & (md['vote_count'].notnull()) & (md['vote_average'].notnull())][['title', 'year', 'vote_count', 'vote_average', 'popularity', 'genres']]
+qualified['vote_count'] = qualified['vote_count'].astype('int')
+qualified['vote_average'] = qualified['vote_average'].astype('int')
+qualified.shape
+
+def weighted_rating(x):
+    v = x['vote_count']
+    R = x['vote_average']
+    return (v/(v+m) * R) + (m/(m+v) * C)
+
+qualified['wr'] = qualified.apply(weighted_rating, axis=1)
+
+qualified = qualified.sort_values('wr', ascending=False).head(250)
+
+s = md.apply(lambda x: pd.Series(x['genres']),axis=1).stack().reset_index(level=1, drop=True)
+s.name = 'genre'
+gen_md = md.drop('genres', axis=1).join(s)
+
+smd = md
+
+smd['description'] = smd['overview']
+smd['description'] = smd['description'].fillna('')
+
+tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+tfidf_matrix = tf.fit_transform(smd['description'])
+
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+smd = smd.reset_index()
+titles = smd['title']
+ids = smd['id']
+indices = pd.Series(smd.index, index=smd['title'])
+
+def get_recommendations(title):
+    if title not in indices:
+        return -1
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:31]
+    movie_indices = [i[0] for i in sim_scores]
+    
+    response = []
+    
+    for idx in ids.iloc[movie_indices].head(10):
+        response.append(idx)
+    
+    return {"response": response}
